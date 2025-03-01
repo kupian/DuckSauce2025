b0fee1a7548daf9d899b91975dab5f80f4ccf1e2
Alex Carr <alexcarr1113@outlook.com>
2025-03-01 14:52:06 +0000

    Player changes

--- DuckSauce2025/scripts/player.gd
+++ DuckSauce2025/scripts/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
+const DASH_SPEED = 5000.0
 
 
 func _physics_process(delta):
@@ -11,22 +12,34 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
-		$AnimatedSprite2D.play("run")
+		if is_on_floor():
+			$AnimatedSprite2D.play("run")
+		else:
+			$AnimatedSprite2D.play("roll")
 		if direction > 0:
 			$AnimatedSprite2D.flip_h = false
 		else:
 			$AnimatedSprite2D.flip_h = true
 		velocity.x = direction * SPEED
+		
+		if Input.is_action_just_pressed("dash") and $Cooldown.is_stopped():		
+			$AnimatedSprite2D.play("dash")
+			velocity.x = direction * DASH_SPEED
+			$Cooldown.start()
+		
 	else:
-		$AnimatedSprite2D.play("idle")
+		if is_on_floor():
+			$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("jump")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
